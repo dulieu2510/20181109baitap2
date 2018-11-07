f6db705efe4741b968e9372a7de48dac9f19e51a
dulieu2510 <giaptg@gmail.com>
2018-11-07 14:39:32 +0700

    4) add meger fix

--- app/src/main/java/giapdev/a20181109baitap2/MainActivity.java
+++ app/src/main/java/giapdev/a20181109baitap2/MainActivity.java
@@ -1,18 +1,31 @@
 package giapdev.a20181109baitap2;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
-    TextView tx1 ;
-    Button bt2;
-
+public class MainActivity extends Activity {
+    EditText tx1 ;
+    Button bt1;
+//fix
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        bt1 = (Button) findViewById(R.id.xbt1);
+        tx1 = (EditText) findViewById(R.id.xtx1);
+        bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intt = new Intent(MainActivity.this,xuatketqua.class );
+                intt.putExtra("ten",new String(tx1.getText().toString()));
+                startActivity(intt);
+            }
+        });
     }
 }
