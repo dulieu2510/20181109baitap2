1bcb75ccaf47f46e2df9adcaa04c2268785cd3fa
dulieu2510 <giaptg@gmail.com>
2018-11-07 12:09:35 +0700

    share full code

---
+++ app/src/main/java/giapdev/a20181109baitap2/MainActivity.java
@@ -0,0 +1,18 @@
+package giapdev.a20181109baitap2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tx1 ;
+    Button bt2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
